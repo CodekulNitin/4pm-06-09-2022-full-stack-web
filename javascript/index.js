8d551f84e8ac0b13b813f4e4b54a0ec829840962
CodekulNitin7872 <codekul.nitin@gmail.com>
2022-10-17 16:59:13 +0530

    basic function

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,20 @@
-let x = 10;
-if(x == 10){
-    let x = 20;
-    console.log(" the value of x is" +x);
-}
-console.log(x);
+// Primitives /values type
+let name ="Codekul"; //string
+let age =25; //number
+let isApproved= false; // boolean
+let firstName = undefined; // undefined
+let selectColor = null; // 
+console.log(typeof name);
+
+ //Cannot be Reserved Keywords
+ //should be meaningfull Names
+ //can not start With Numbers (1Name)
+ // cannot contain a space or hyphen
+ // are case Sensitive
+
+
+
+
+
+
+// Refrence Types
