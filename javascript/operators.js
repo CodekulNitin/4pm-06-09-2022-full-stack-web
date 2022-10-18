3cf8cba9c987e4cb83d78ee308efed4d76861de9
CodekulNitin7872 <codekul.nitin@gmail.com>
2022-10-18 16:56:33 +0530

    ..push

---
+++ javascript/operators.js
@@ -0,0 +1,41 @@
+//arithamatic operators 
+// let x = 10;
+
+
+// compound Operator
+// x+=y; // x=x + y
+// x += 1;
+// x++
+// console.log(x);
+
+// logical operetors
+ 
+// 1. logical AND && 
+//  let happy = true && true;
+
+//  console.log(happy);
+
+// const isLoggedIn = true;
+// const hasPermissions = true;
+
+// if(isLoggedIn && hasPermissions ){
+//     console.log("your data is sucessfully deleted");
+// }else{
+//     console.log("there is no permission ");
+// }
+// console.log("cat" && "dog");
+ 
+
+// logical  OR ||
+// const happy = true || false;
+// console.log(happy);
+//   logical NOT !
+
+// const happy = false
+// console.log(!happy);
+
+// const  isLoggedIn = false;
+// if(!isLoggedIn){
+//     console.log("redirect to login page");
+// }
+
